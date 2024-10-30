d601fbf9c8b311aaa3b97d0c2867a8c02ad9481f
DamoFD <voshall@yahoo.com>
2024-10-29 21:27:22 -0700

    fix docker and update php comments

--- www/database.php
+++ www/database.php
@@ -1,25 +1,87 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+* Database class creates and checks the database connection
+*
+* @since 1.0
+*/
 class Database
 {
+    /**
+    * The database host name
+    *
+    * @var string
+    *
+    * @since 1.0
+    */
     private string $servername = "db";
-    private string $username = "root";
-    private string $password = "password";
-    //uncomment me private string $dbname = "your database name";
-    public object $conn;
 
+    /**
+    * The database username
+    *
+    * @var string
+    *
+    * @since 1.0
+    */
+    private string $username = "user";
+
+    /**
+    * The database password
+    *
+    * @var string
+    *
+    * @since 1.0
+    */
+    private string $password = "password";
+
+    /**
+    * The database name
+    *
+    * @var string
+    *
+    * @since 1.0
+    */
+    private string $dbname = "test";
+
+    /**
+    * The database connection object
+    *
+    * @var mysqli
+    *
+    * @since 1.0
+    */
+    public mysqli $conn;
+
+    /**
+    * Database constructor
+    *
+    * @throws Exception
+    *
+    * @since 1.0
+    */
     public function __construct()
     {
-        $this->conn = new mysqli($this->servername, $this->username, $this->password); //comment/remove
-        //uncomment me $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        // Create database connection
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
 
+        // Check connection errors
         if ($this->conn->connect_error) {
             throw new Exception("Connection failed: " . $this->conn->connect_error);
         }
     }
 
+    /**
+    * Checks if database is connected
+    *
+    * @return bool
+    *
+    * @since 1.0
+    */
     public function isDbConnected(): bool
     {
+        // Ping the database
         return $this->conn->ping();
     }
 }
--- www/index.php
+++ www/index.php
@@ -2,13 +2,16 @@
 
 require 'database.php';
 
+// Initialize variables
 $connection = 'Database Not Connected';
 $isConnected = false;
 
 try {
+    // Initialize new database class
     $database = new Database();
     $isConnected = $database->isDbConnected();
 
+    // Check if database is connected
     if ($isConnected) {
         $connection = "Database Connected";
     }
@@ -28,25 +31,28 @@ try {
         <h2>If you can see this on your browser, your docker container is running successfully!</h2>
         <p>Here is a check to see if your database is connected:</p>
 
+        <!-- Check if database is connected -->
         <h2 style="color: <?php echo $isConnected ? 'green' : 'red'; ?>;"><?= $connection ?></h2>
         <br />
 
+        <!-- PHPMyAdmin -->
         <p>PHPMyAdmin will be running on <a href="http://localhost:8080/">http://localhost:8080</a>.</p>
         <p>Default credentials:</p>
 
+        <!-- Database Credentials -->
         <ul>
             <li>Server: db</li>
-            <li>Username: root</li>
+            <li>Username: user</li>
             <li>Password: password</li>
         </ul>
         <br />
 
-        <h2>Add your Database</h2>
+        <h2>Change Database Info</h2>
 
         <ul>
-            <li>After making your database, uncomment the <code>$dbname</code> from the database.php file and input the name of your database.</li>
-            <li>Comment/Remove the old <code>new mysqli()</code></li>
-            <li>Uncomment the new <code>new mysqli()</code></li>
+            <li>Edit the docker-compose.yml file</li>
+            <li>Feel free to update the environment variables</li>
+            <li>Update those changes in the Database.php file</li>
         </ul>
         <br />
 
